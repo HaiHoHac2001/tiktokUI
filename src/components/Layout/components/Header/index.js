52c795f833a2d54994e8a5715923ebb523e7b9dc
Ho Hac Hai <hohachai162001@gmail.com>
2022-12-21 13:44:44 +0700

    # add logic for menu multi level

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,16 +1,119 @@
-import classNames from 'classnames/bind'
-import styles from './Header.module.scss'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 
-const cx = classNames.bind(styles)
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faPlus, faEllipsisV, faLanguage, faCircleQuestion, faKeyboard, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AcountItem from '~/components/AcountItem';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faLanguage} />,
+        title: 'Tiếng Việt',
+        children: {
+            title: "Ngôn ngữ",
+            data: [
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                    
+                },
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+            ]
+        }
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Phản hồi & trợ giúp',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Phím tắt trên bàn phím'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faMoon} />,
+        title: 'Chế độ tối'
+    },
+]
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/* logo */}
-            {/* search */}
-            {/* action */}
-        </div>
-    </header>
+    const [searchResult, setSeachResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSeachResult([]);
+        }, 0);
+    }, []);
+
+    // Handle logic
+    const handleOnchangeMenu = (menuItems) => {
+        console.log(menuItems)
+    }
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* logo */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                {/* search */}
+                <Tippy
+                    interactive={true}
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <div className={cx('search-title')}>Tài khoản</div>
+                                <AcountItem />
+                                <AcountItem />
+                                <AcountItem />
+                                <AcountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        {/* loading */}
+                        {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                {/* action */}
+                <div className={cx('action')}>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        Tải lên
+                    </Button>
+                    <Button primary>Đăng nhập</Button>
+                    <Menu items={MENU_ITEMS} onChange={handleOnchangeMenu}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisV} />
+                        </button>
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
